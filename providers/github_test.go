b748a7c497619c7e4ad7536782382cdf3fb2b64f
nbedos <nicolas.bedos@gmail.com>
2019-11-23 18:46:00 +0100

    Add local test for GitHub

--- providers/github_test.go
+++ providers/github_test.go
@@ -2,16 +2,52 @@ package providers
 
 import (
 	"context"
-	"os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-github/v28/github"
 )
 
 func TestClient(t *testing.T) {
-	var token *string
-	if env := os.Getenv("GITHUB_API_TOKEN"); env != "" {
-		token = &env
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		filename := ""
+		switch r.URL.Path {
+		case "/repos/nbedos/termtosvg/commits/d58600a58bf1738c6529ce3489a546bfa2178e07/check-runs":
+			filename = "github_check_runs.json"
+		case "/repos/nbedos/termtosvg/commits/d58600a58bf1738c6529ce3489a546bfa2178e07/statuses":
+			filename = "github_statuses.json"
+		default:
+			w.WriteHeader(404)
+			return
+		}
+
+		bs, err := ioutil.ReadFile(fmt.Sprintf("test_data/%s", filename))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if _, err := fmt.Fprint(w, string(bs)); err != nil {
+			t.Fatal(err)
+		}
+	}))
+	defer ts.Close()
+
+	tsu, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
 	}
-	client := NewGitHubClient(context.Background(), token)
+
+	c, err := github.NewEnterpriseClient(tsu.String(), "example.com", ts.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := GitHubClient{
+		client: c,
+	}
 	owner := "nbedos"
 	repo := "termtosvg"
 	sha := "d58600a58bf1738c6529ce3489a546bfa2178e07"
@@ -21,7 +57,20 @@ func TestClient(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	buildURLs := map[string]struct{}{}
 	for _, u := range urls {
-		t.Log(u)
+		buildURLs[u] = struct{}{}
+	}
+
+	expectedURLs := map[string]struct{}{
+		"https://circleci.com/gh/nbedos/citop/36":                      {},
+		"https://ci.appveyor.com/project/nbedos/citop/builds/29024796": {},
+		"https://travis-ci.com/owner/repository/builds/123654789":      {},
+		"https://travis-ci.org/nbedos/citop/builds/615087280":          {},
+		"https://gitlab.com/nbedos/citop/pipelines/97604657":           {},
+	}
+
+	if diff := cmp.Diff(buildURLs, expectedURLs); len(diff) > 0 {
+		t.Fatal(diff)
 	}
 }
